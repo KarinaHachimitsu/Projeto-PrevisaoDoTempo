449a8197f66d0b0012b454fc53e454d1f719f227
Karina Mel <92555432+KarinaHachimitsu@users.noreply.github.com>
2022-01-03 11:46:19 -0300

    Estrutura Basica do Projeto

---
+++ index.js
@@ -0,0 +1,9 @@
+const h1 = document.querySelector('.h1')
+
+const input = document.querySelector('.input')
+const btn = document.querySelector('.btn')
+const divResult = document.createElement('div')
+divResult.className = 'teste'
+const sectionResult = document.querySelector('.resultado')
+sectionResult.appendChild(divResult)
+//divResult é uma const por isso não se usa ''
